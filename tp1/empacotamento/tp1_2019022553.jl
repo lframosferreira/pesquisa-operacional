b7905ac7c6a39f19428a9802d87c0a600081becc
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-09-29 23:24:28 -0300

    primeiro problema

---
+++ tp1/empacotamento/tp1_2019022553.jl
@@ -0,0 +1,14 @@
+using JuMP
+using HiGHS
+
+mutable struct empacotamentoData
+  n::Int # número de objetos
+  weights::Array{Int} # Pesos dos objetos
+end 
+
+function readData()
+  println("vasco")
+end 
+
+readData()
+
