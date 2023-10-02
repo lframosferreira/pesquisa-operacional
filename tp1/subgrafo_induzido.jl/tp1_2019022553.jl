ce8b679ceab99d92878902414af366a560ad4706
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-10-02 20:30:41 -0300

    tentando subgrafo induzido

--- tp1/subgrafo_induzido.jl/tp1_2019022553.jl
+++ tp1/subgrafo_induzido.jl/tp1_2019022553.jl
@@ -1,9 +1,10 @@
 using JuMP
 using HiGHS
 
-mutable struct coloracaoData
+mutable struct subgrafoInduzidoData
 	numberOfVertices::Int64 # número de vértices
 	adjMatrix::Matrix{Int64} # Matriz de adjacências
+    weightsMatrix::Matrix{Float64}
 end
 
 function readData(file)
@@ -13,15 +14,19 @@ function readData(file)
 		q = split(line, "\t")
 		if (q[1] == "n")
 			numberOfVertices = parse(Int64, q[2])
-			adjMatrix = zeros(Int64, numberOfVertices, numberOfVertices)
+            adjMatrix = zeros(Int64, numberOfVertices, numberOfVertices)
+			weightsMatrix = zeros(Float64, numberOfVertices, numberOfVertices)
 		elseif q[1] == "e"
 			v1 = parse(Int64, q[2])
             v2 = parse(Int64, q[3])
+            w = parse(Float64, q[4])
 			adjMatrix[v1, v2] = 1
             adjMatrix[v2, v1] = 1
+            weightsMatrix[v1, v2] = w
+            weightsMatrix[v2, v1] = w
 		end
 	end
-	return coloracaoData(numberOfVertices, adjMatrix)
+	return subgrafoInduzidoData(numberOfVertices, adjMatrix)
 end
 
 model = Model(HiGHS.Optimizer)
@@ -30,32 +35,21 @@ file = open(ARGS[1], "r")
 
 data = readData(file)
 
-# Podemos assumir que existem N cores. Ci indica que a cor i está sendo utilizada
-@variable(model, c[i = 1:data.numberOfVertices], Bin)
+# Vértice i pertence ao conjunto S
+@variable(model, S[i = 1:data.numberOfVertices], Bin)
 
-# Vértice i está colorido com a cor j
+# Aresta ij está no subgrafo induzido
 @variable(model, x[i = 1:data.numberOfVertices, j = 1:data.numberOfVertices], Bin)
 
-
-# Cada vértice deve estar colorido com exatamente uma cor
-for i in 1:data.numberOfVertices
-    @constraint(model, sum(x[i, j] for j in 1:data.numberOfVertices) == 1)
-end
-
-# Se a aresta ij existe em E(G), as cores de i e j devem ser diferentes
 for i in 1:data.numberOfVertices
     for j in 1:data.numberOfVertices
-        if (data.adjMatrix[i, j] == 1)
-            for color in 1:data.numberOfVertices
-	            @constraint(model, x[i,color] + x[j, color] <= c[color])
-            end
-        end
+	    @constraint(model, S[i] + S[j] >= 2 * adjMatrix[i, j])
     end
 end
 
 
 # Minimizar o número de cores utilizadas
-@objective(model, Min, sum(c[i] for i ∈ 1:data.numberOfVertices))
+@objective(model, Min, sum(S[i] * data.adjMatrix[i] for i ∈ 1:data.numberOfVertices))
 
 print(model)
 
