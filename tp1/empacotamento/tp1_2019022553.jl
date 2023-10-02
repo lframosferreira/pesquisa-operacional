a494fddc70a0b270f38f6cce4f180c171bb158e0
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-10-01 23:24:41 -0300

    avanços no primeiro problema

--- tp1/empacotamento/tp1_2019022553.jl
+++ tp1/empacotamento/tp1_2019022553.jl
@@ -23,7 +23,7 @@ end
 
 function printSolution(data, x)
 	println("Uma Cobertura Mínima:")
-	for i = 1: data.n
+	for i ∈ 1:data.n
 		if value(x[i]) > 0.5
 			println("$i")
 		end
@@ -37,8 +37,23 @@ file = open(ARGS[1], "r")
 
 data = readData(file)
 
-@variable(model, x[i=1:data.n], Bin)
+@variable(model, x[i = 1:data.n][i = 1:data.n], Bin)
 
-for i=1:data.n
-	@constraint(model, sum(x[j] for j in data.ng[i]) >= 1)	
+# cada objeto deve estar em exatamente uma caixa
+for i in 1:data.n
+	@constraint(model, sum(x[i][j] for j in 1:data.n) == 1)
 end
+
+# soma nas caixas menor que 20
+for j in 1:data.n
+	@constraint(model, sum(data.weights[i] * x[i][j] for i ∈ 1:data.n) <= 20)
+end
+
+@objective(model, Min, sum(x[i] for i ∈ 1:data.n))
+
+print(model)
+
+optimize!(model)
+
+sol = objective_value(model)
+println("Valor otimo= ", sol)
