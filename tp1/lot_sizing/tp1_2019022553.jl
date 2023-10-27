f81941eb9875e1058cc260e4edbd83b77c04257c
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-10-27 09:25:43 -0300

    quase acabando o tp1

--- tp1/lot_sizing/tp1_2019022553.jl
+++ tp1/lot_sizing/tp1_2019022553.jl
@@ -44,19 +44,50 @@ file = open(ARGS[1], "r")
 data = readData(file)
 
 # prod[i] é a produção do período i
-@variable(model, prod[i=1:data.numberOfPeriods])
+@variable(model, prod[i=1:data.numberOfPeriods] >= 0)
 
-# est[i] é o estoque do período i - 1 pro periodo i, isto é, quanto eu tenho em estoque pro período i
-# assume-se que p[0] é 0
-@variable(model, est[i=1:data.numberOfPeriods])
+# est[i] é o quanto eu tenho em estoque pro período i
+# assume-se que est[0] é 0
+@variable(model, est[i=1:data.numberOfPeriods+1] >= 0)
 
-# debt[i] é o quanto estou devendo ao acabar o período i
-@variable(model, est[i=1:data.numberOfPeriods])
+# devo[i] é o quanto estou devendo no periodo i
+@variable(model, devo[i=1:data.numberOfPeriods+1] >= 0)
 
-@objective(model, Min, 3)
+# pago[i] é o quanto paguei no período i
+@variable(model, pago[i=1:data.numberOfPeriods] >= 0)
+
+# nunca posso pagar mais do que devo somado a demanda atual (teria estoque negativo)
+for i in 1:data.numberOfPeriods
+  @constraint(model, pago[i] <= devo[i] + data.demand[i])
+end
+
+# nunca posso pagar mais do que tenho
+for i in 1:data.numberOfPeriods
+  @constraint(model, pago[i] <= prod[i] + est[i])
+end
+
+@constraint(model, devo[1] == 0)
+@constraint(model, est[1] == 0)
+@constraint(model, devo[data.numberOfPeriods+1] == 0)
+
+# est pro proximo periodo é prod + est - pago
+for i in 1:data.numberOfPeriods
+  @constraint(model, est[i+1] == prod[i] + est[i] - pago[i])
+end
+
+# devo é qnt devia antes + demanda menos pago
+for i in 1:data.numberOfPeriods
+  @constraint(model, devo[i+1] == devo[i] + data.demand[i] - pago[i])
+end
+
+@objective(model, Min, sum(prod[i] * data.productionCost[i] + est[i+1] * data.storageCost[i] + devo[i] * data.assessmentCost[i] for i in 1:data.numberOfPeriods))
 
 optimize!(model)
 
 sol = objective_value(model)
 println("TP1 2019022553 = ", sol)
+
+for i in 1:data.numberOfPeriods
+  println("$(value(prod[i]))")
+end
 
